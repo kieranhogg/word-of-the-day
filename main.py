3c52619b0069073018f6a811eb8276e0cc13d82a
kieran <kieran.hogg@gmail.com>
2026-04-23 20:22:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Word(BaseModel):
     word: str
     definition: str
